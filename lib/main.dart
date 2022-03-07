d4817a7067959530551dbcf1eb3aaf0a12164819
tku-peiyucheng <peiyu@gms.tku.edu.tw>
2022-03-07 12:23:08 +0800

    Hi, Flutter

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // 建立appTitle和hiFlutter物件
+  var appTitle = Text('我的第一個Flutter App'),
+      hiFlutter = Text(
+        'Hi, Flutter.',
+        style: TextStyle(fontSize: 50),
+      );
+
+  // 建立appBody物件
+  var appBody = Center(
+    child: hiFlutter,
+  );
+
+  // 建立appBar物件
+  var appBar = AppBar(
+    title: appTitle,
+  );
+
+  // 建立app物件
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: appBar,
+      body: appBody,
+    ),
+  );
+
+  runApp(app);
+}
